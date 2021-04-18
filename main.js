d73474a732c87550a06020d614f2f764f907a730
Scott-yang-21 <81839731+Scott-yang-21@users.noreply.github.com>
2021-04-17 16:29:42 -1000

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 var h = window.innerHeight
 var w = window.innerWidth
-var player;
+//var player;
 var sprt;
 
 function setup() {
     createCanvas(w, h);
     background(0);
     galaxy = loadImage("galaxy.png")
-    image(galaxy, w/2, h/2, width, height)
-    player.update();
+    Image(galaxy, w/2, h/2, width, height)
+    //player.update();
     var sprt = createSprite(height/2, width/2, 50, 50);
     sprt.shapeColor = color(255);
-    player = new player();
+    //player = new player();
 }
 
 function draw() {
@@ -21,7 +21,7 @@ function draw() {
 function player() {
     this.x = w/2;
     this.y = h/2;
-    this.update = function() {
+    //this.update = function() {
         
-    }
+    //}
 }
