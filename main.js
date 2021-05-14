d480280f422b463ef1553083eb32fcb8c6c99c53
Scott-yang-21 <81839731+Scott-yang-21@users.noreply.github.com>
2021-05-13 23:52:25 -1000

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,18 @@ var tentacles;
 var sprt;
 var player;
 var boss;
+var restart;
 var arrow;
 var circ;
 var r_circle;
 var wave = 1;
 var death_status = 0;
 var startup = 0;
+var goal = 1;
 
 function setup() {
   createCanvas(w, h);
-
+  
   circ = loadImage('https://scott-yang-21.github.io/Circle_Blue.png');
   r_circle = loadImage('https://scott-yang-21.github.io/Circle_Red4.png');
   arrow = loadImage('https://scott-yang-21.github.io/arrow.png')
@@ -26,11 +28,12 @@ function setup() {
   
   player = new player();
   boss = new boss();
+  restart = new restart();
   
   bullets = new Group();
   tentacles = new Group();
 
-  var sprtb = createSprite(200+(100*i), height);
+  var sprtb = createSprite(200+(100*i), height*1.2);
   sprtb.addImage(r_circle);
   sprtb.setCollider('circle', 0, 0, 275);
   sprtb.scale = 0.3;
@@ -38,7 +41,7 @@ function setup() {
   tentacles.add(sprtb);
 
   for (var i = 1; i<4; i++) {
-    var sprtb = createSprite(200+(100*i), height);
+    var sprtb = createSprite(200+(100*i), height*1.2);
     sprtb.addImage(circ);
     sprtb.setCollider('circle', 0, 0, 340);
     sprtb.scale = 0.3;
@@ -67,26 +70,113 @@ function draw() {
     text("Control Character with Arrow Keys", width/2, height*0.55)
     text("Press SPACEBAR to Shoot", width/2, height*0.6)
     text("Press 'k' to start", width/2, height*0.7)
-    
+    restart.goal();
   }
+
   else {
-    startup = 1;
-    boss.revive();
-    drawSprites();
-    boss.update();
-    tentacles.displace(tentacles);
-    player.border();
-    player.movement();
-    player.shoot();
-    player.death();
-    boss.b_life();
-    boss.b_move();
-    boss.b_border();
-  
+    if (death_status==0) {
+      startup = 1;
+      goal = 0;
+      boss.revive();
+      drawSprites();
+      boss.update();
+      tentacles.displace(tentacles);
+      player.border();
+      player.movement();
+      player.shoot();
+      player.death();
+      boss.b_life();
+      boss.b_move();
+      boss.b_border();
+    }
+    
+    else {
+      restart.restarting();
+      death_status = 0;
+      startup = 0;
+    }
+
     textSize(25);
     fill(255);
     textAlign(CENTER, CENTER);
     text("Wave: "+wave, width/2, h/20);
+  }
+}
+
+function restart() {
+  this.restarting = function() {
+    
+    /*sprt = createSprite(width/2, height/2);
+    sprt.addImage(arrow);
+    sprt.scale = 0.1;
+    sprt.maxSpeed = 9;
+    sprt.friction = 0.05;
+
+    sprt.position.x = width/2;
+    sprt.position.y = height/2;
+
+    //attempting to change respawn method
+    for (var j=0; j<5; j++) {
+      tentacles[j].remove()
+    }
+
+    var sprtb = createSprite(200+(100*i), height);
+    sprtb.addImage(r_circle);
+    sprtb.setCollider('circle', 0, 0, 275);
+    sprtb.scale = 0.3;
+    sprtb.mass = sprtb.scale;
+    tentacles.add(sprtb);
+
+    for (var i = 1; i<4; i++) {
+      var sprtb = createSprite(200+(100*i), sprt.position.y+600);
+      sprtb.addImage(circ);
+      sprtb.setCollider('circle', 0, 0, 340);
+      sprtb.scale = 0.3;
+      sprtb.mass = sprtb.scale;
+      tentacles.add(sprtb);
+    }
+
+    for (var c = 0; c<tentacles.length; c++) {
+      //this.drag -= 0.005;
+      tentacles[c].maxSpeed = 5;
+      tentacles[c].friction = 0.2;
+    }*/
+
+    sprt = createSprite(width/2, height/2);
+    sprt.addImage(arrow);
+    sprt.scale = 0.1;
+    sprt.maxSpeed = 9;
+    sprt.friction = 0.05;
+
+    sprt.position.x = width/2;
+    sprt.position.y = height/2;
+
+    for (var y=0; y<tentacles.length; y++) {
+      tentacles[y].position.x = 200+(150*y); //changed '100' to '150'
+      tentacles[y].position.y = height;
+      tentacles[y].scale = 0.3;
+      tentacles[y].maxSpeed = 5;
+      boss.drag = 0.2
+      tentacles[y].friction = boss.drag;
+    }
+
+    boss.hp1 = [3, 3, 3, 3];
+    wave = 1;
+  }
+
+  this.goal = function() {
+    if (goal == 0) {
+      fill(255);  
+      textSize(25);
+      textAlign(CENTER, CENTER);
+      if (this.progress < wave) {
+        this.progress = wave;
+      }
+      text("The longest you've lasted is Wave " + this.progress, width/2, height*0.9)
+    }
+    else {
+      this.progress = wave;
+    }
   }
 }
 
@@ -152,7 +242,7 @@ function player() {
         textSize(25);
         text("reload page to play again", width/2, height*0.6)
         death_status = 1;
-        
+        startup = 0;
       }
     }
   }
@@ -217,7 +307,8 @@ function boss() {
       sprtb.scale = 0.3;
       sprtb.mass = sprtb.scale;
       tentacles.add(sprtb);
-  
+    
+      
       for (var i = 1; i<4; i++) {
         var sprtb = createSprite(200+(100*i), sprt.position.y+600);
         sprtb.addImage(circ);
@@ -243,4 +334,6 @@ function boss() {
     }
     this.life_l = tentacles.length;
   }
+
+
 }
